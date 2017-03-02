3ec81061dd97045492de802a060c5c8a7b4af13d
elaine lim <lainex@gmail.com>
2017-03-02 19:10:46 +0800

    created welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'welcome/index'
+
   get 'parks/index'
   get 'parks/:id', to: 'parks#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :parks
-  root "parks#index"
+  root "welcome#index"
 
 end
