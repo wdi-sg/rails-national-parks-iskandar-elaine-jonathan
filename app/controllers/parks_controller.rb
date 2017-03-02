4f697eaeeab409b97d6a51d9a06c60ba1b62b9d7
Jonathan Lim <jonathanlimes@gmail.com>
2017-03-02 18:24:35 +0800

    homework done

---
+++ app/controllers/parks_controller.rb
@@ -0,0 +1,11 @@
+class ParksController < ApplicationController
+
+  def index
+    @parks = Park.all
+  end
+
+  def show
+    @park = Park.find(params[:id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+  get 'parks/index'
+  get 'parks/:id', to: 'parks#show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :parks
+  root "parks#index"
+
 end
