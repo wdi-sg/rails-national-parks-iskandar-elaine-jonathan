7b67b33868046f8748bb4b869370a157819ed2cc
Mohammad Noh Iskandar Jamaluddin <anatraxia@gmail.com>
2017-03-03 11:06:23 +0800

    add new seed record

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,5 @@
 park2 = Park.create(name: "Macritchie Reservoir", description: "everyone comes here to run", picture:"http://www.yoursingapore.com/see-do-singapore/nature-wildlife/reserves/macritchie-nature-reserve/_jcr_content/par-carousel/carousel_detailpage/carousel/item_1.thumbnail.carousel-img.740.416.jpg")
 
 park3 = Park.create(name: "Bukit Timah Hill", description: "everyone comes here to walk", picture:"https://upload.wikimedia.org/wikipedia/commons/1/18/Bukit_timah_rock.jpg")
+
+park4 = Park.create(name: "Gardens by the Bay", description: "everyone comes here to walk", picture:"http://www.gardensbythebay.com.sg/content/gbb/en/attractions/supertree-grove/_jcr_content/par-overview/cq_col_control/columnLeft/cq_content_carousel/carouselimagefield/item_1.renderimage.imagepath.550.309.jpg")
