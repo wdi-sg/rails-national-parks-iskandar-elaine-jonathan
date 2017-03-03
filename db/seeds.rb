bc7f654a9a7de2684ed491ed0900ea9b477e9ba0
Jonathan Lim <jonathanlimes@gmail.com>
2017-03-03 09:16:22 +0800

    created seeds in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+park1 = Park.create(name: "Macritchie Reservoir", description: "everyone comes here to run", picture:"http://www.yoursingapore.com/see-do-singapore/nature-wildlife/reserves/macritchie-nature-reserve/_jcr_content/par-carousel/carousel_detailpage/carousel/item_1.thumbnail.carousel-img.740.416.jpg")
